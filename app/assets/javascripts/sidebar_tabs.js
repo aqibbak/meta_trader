9511f89f50ae5e9ebb86a764e6faad0a5df9740f
NKolosek <dev@kolosek.com>
2013-05-14 13:50:52 +0200

    implementing sidebar tabs functionality

---
+++ app/assets/javascripts/sidebar_tabs.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    /* TODO: finishme! */
+
+    /* Init */
+    // hide_all_tabs();
+    // $('.common_tab').show();
+
+    var hide_all_tabs = function() {
+        $('.tab').each(function(index, item) { $(item).hide(); });
+    }
+});
