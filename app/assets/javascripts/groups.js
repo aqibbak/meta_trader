b25adb80be00d76a85b4a2b6955332d0e117cb45
mmilosevic <marko.milosevic@kolosek.com>
2013-05-22 16:13:11 +0200

    Added group tab

---
+++ app/assets/javascripts/groups.js
@@ -0,0 +1,28 @@
+function fetchGroup(pos){
+  $.ajax({
+    url: '/fetch_group',
+    data:  { pos: pos },
+    dataType: 'script'
+  }).success(function(data, status, xhr) {
+  });
+}
+
+function secgroupEvents(){
+  $(".secgroup-row").click(function(){
+    var pos = $(this).data().pos;
+    $(".secgroup-div").hide();
+    $("#secgroup" + pos).show();
+  });
+}
+
+function initGroupEvents(){
+  $(".group-row").click(function(){
+    var pos = $(this).data().pos;
+    fetchGroup(pos);
+  });
+  secgroupEvents();
+}
+
+$(document).ready(function(){
+  initGroupEvents();
+});
