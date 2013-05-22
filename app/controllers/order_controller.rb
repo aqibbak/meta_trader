767bf94e5a89c9364e2c487218fc9db71e517086
mmilosevic <marko.milosevic@kolosek.com>
2013-05-22 16:13:41 +0200

    Added order tab

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,31 @@
+class OrderController < ApplicationController
+  include ApplicationHelper
+
+  def filter_order
+    authorize! :read, :order
+    filter = params[:order_filter]
+    api = ApiMethods.new
+    @account = api.getOrder(session,filter)
+  end
+
+  def order_submit
+    authorize! :update, :order
+    data = params[:order]
+    api = ApiMethods.new
+    @result = api.updateOrder(data,session) unless data.nil?
+    render :nothing => true
+  end
+
+  def fetch_order
+    authorize! :read, :order
+    order = params[:order]
+    api = ApiMethods.new
+    filter = { "ticket" => {"condition" => "=", "value" => order }}
+    @order = api.getOrder(session)
+    @symbol = api.getSymbol(session)
+
+    @ord = @order.first
+    puts @ord
+  end
+
+end
