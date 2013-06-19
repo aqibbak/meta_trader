a810a118f7a3f5f49837bb9f3f647935ed55160e
NKolosek <office@kolosek.com>
2013-06-19 14:37:28 +0200

    Don't add Devise users to existing users

--- db/migrate/20130619073237_add_username_to_users.rb
+++ db/migrate/20130619073237_add_username_to_users.rb
@@ -2,16 +2,6 @@ class AddUsernameToUsers < ActiveRecord::Migration
   def up
     add_column :users, :username, :string
 
-    User.all.each do |user|
-      if user.username.nil?
-        if user.admin?
-          user.update_attribute(:username, "admin" + user.id.to_s)
-        else
-          user.update_attribute(:username, "user" + user.id.to_s)
-        end
-      end
-    end
-
     change_column :users, :username, :string, :null => false
 
     add_index :users, :username, :unique => true
